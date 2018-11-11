0083023241dc2b669ca5742d7bce3eae8c4a54e5
Unknown <poicaptiondx@gmail.com>
2018-11-11 10:18:24 -0800

    midt

---
+++ routes/web.php
@@ -0,0 +1,56 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('home', function () {
+    return view('home');
+});
+
+Route::get('shop', function () {
+    return view('shop');
+});
+
+Route::get('register', function () {
+    return view('register');
+});
+
+Route::get('login', function () {
+    return view('login');
+});
+
+Route::get('cart', function () {
+    return view('cart');
+});
+
+Route::get('about', function () {
+    return view('about');
+});
+
+Route::get('contact', function () {
+    return view('contact');
+});
+
+Route::get('checkout', function () {
+    return view('checkout');
+});
+
+Route::get('thankyou', function () {
+    return view('thankyou');
+});
+
+Route::get('shopsingle', function () {
+    return view('shopsingle');
+});
